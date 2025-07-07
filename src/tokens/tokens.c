1c03c7f0ea8b1b8a697fe8c943c4642546f45d4b
Vitor Correa w10 <correavitor4@hotmail.com>
2025-07-07 13:01:24 +0000

    feat: implement all tokens

--- src/tokens/tokens.c
+++ src/tokens/tokens.c
@@ -3,23 +3,121 @@
 #include <cstdio>
 #include <ctype.h>  // para isalpha, isdigit
 
-#pragma region reserved keywords definition
-#pragma endregion
-
 //TODO: testar
 int try_parse_token(char *token_string, int token_string_length, Token *token)
 {
+    TokenType *token_type = &token->type;
 
+    // 1. Verifica se é um token de tipo de variável
+    int result = try_parse_data_data(token_string, token_type);
+    if (result == TRY_PARSE_DATA_TYPE_TOKEN_SUCCESS) {
+        token->literal = allocate_memory(token_string_length + 1);
+        if (token->literal == NULL) {
+            fprintf(stderr, "Erro: falha ao alocar memória para o literal do token.\n");
+            return TRY_PARSE_DATA_TYPE_TOKEN_ERROR;
+        }
+        memcpy(token->literal, token_string, token_string_length);
+        token->literal[token_string_length] = '\0';
+        return TRY_PARSE_DATA_TYPE_TOKEN_SUCCESS;
+    }
+
+    // 2. Verifica se é um token de função
+    result = try_parse_functions(token_string, token_string_length, token_type);
+    if (result == TRY_PARSE_FUNCTION_TOKEN_SUCCESS) {
+        token->literal = allocate_memory(token_string_length + 1);
+        if (token->literal == NULL) {
+            fprintf(stderr, "Erro: falha ao alocar memória para o literal do token.\n");
+            return TRY_PARSE_FUNCTION_TOKEN_ERROR;
+        }
+        memcpy(token->literal, token_string, token_string_length);
+        token->literal[token_string_length] = '\0';
+        return TRY_PARSE_FUNCTION_TOKEN_SUCCESS;
+    }
+
+    // 3. Verifica se é um token de operações de console
+    result = try_parse_console_ops(token_string, token_string_length, token_type);
+    if (result == TRY_PARSE_CONSOLE_OPS_TOKEN_SUCCESS) {
+        token->literal = allocate_memory(token_string_length + 1);
+        if (token->literal == NULL) {
+            fprintf(stderr, "Erro: falha ao alocar memória para o literal do token.\n");
+            return TRY_PARSE_CONSOLE_OPS_TOKEN_ERROR;
+        }
+        memcpy(token->literal, token_string, token_string_length);
+        token->literal[token_string_length] = '\0';
+        return TRY_PARSE_CONSOLE_OPS_TOKEN_SUCCESS;
+    }
+
+    // 4. Verifica se é um token de senão condicional
+    result = try_parse_conditional(token_string, token_string_length, token_type);
+    if (result == TRY_PARSE_CONDITIONAL_TOKEN_SUCCESS) {
+        token->literal = allocate_memory(token_string_length + 1);
+        if (token->literal == NULL) {
+            fprintf(stderr, "Erro: falha ao alocar memória para o literal do token.\n");
+            return TRY_PARSE_CONDITIONAL_TOKEN_ERROR;    
+        }
+        memcpy(token->literal, token_string, token_string_length);
+        token->literal[token_string_length] = '\0';
+        return TRY_PARSE_CONDITIONAL_TOKEN_SUCCESS;
+    }
+
+    // 5. Verifica se é um token de para
+    result = try_parse_for_loop(token_string, token_string_length, token_type);
+    if (result == TRY_PARSE_FOR_LOOP_TOKEN_SUCCESS) {
+        token->literal = allocate_memory(token_string_length + 1);
+        if (token->literal == NULL) {
+            fprintf(stderr, "Erro: falha ao alocar memória para o literal do token.\n");
+            return TRY_PARSE_FOR_LOOP_TOKEN_ERROR;
+        }
+        memcpy(token->literal, token_string, token_string_length);
+        token->literal[token_string_length] = '\0';
+        return TRY_PARSE_FOR_LOOP_TOKEN_SUCCESS;
+    }
+
+    // 6. Verifica se é um token de nome de função
+    result = try_parse_function_name(token_string, token_string_length, token_type);
+    if (result == TRY_PARSE_FUNCTION_NAME_TOKEN_SUCCESS) {
+        token->literal = allocate_memory(token_string_length + 1);
+        if (token->literal == NULL) {
+            fprintf(stderr, "Erro: falha ao alocar memória para o literal do token.\n");
+            return TRY_PARSE_FUNCTION_NAME_TOKEN_ERROR;
+        }
+        memcpy(token->literal, token_string, token_string_length);
+        token->literal[token_string_length] = '\0';
+        return TRY_PARSE_FUNCTION_NAME_TOKEN_SUCCESS;
+    }
+
+    // 7. Verifica se é um token de variável
+    result = try_parse_variable(token_string, token_string_length, token_type);
+    if (result == TRY_PARSE_VARIABLE_TOKEN_SUCCESS) {
+        token->literal = allocate_memory(token_string_length + 1);
+        if (token->literal == NULL) {
+            fprintf(stderr, "Erro: falha ao alocar memória para o literal do token.\n");
+            return TRY_PARSE_VARIABLE_TOKEN_ERROR;
+        }
+        memcpy(token->literal, token_string, token_string_length);
+        token->literal[token_string_length] = '\0';
+        return TRY_PARSE_VARIABLE_TOKEN_SUCCESS;
+    }
+
+    // 8. Verifica se é um token de special token
+    result = try_parse_special_token(token_string, token_string_length, token_type);
+    if (result == TRY_PARSE_SPECIAL_TOKEN_SUCCESS) {
+        token->literal = allocate_memory(token_string_length + 1);
+        if (token->literal == NULL) {
+            fprintf(stderr, "Erro: falha ao alocar memória para o literal do token.\n");
+            return TRY_PARSE_SPECIAL_TOKEN_ERROR;
+        }
+        memcpy(token->literal, token_string, token_string_length);
+        token->literal[token_string_length] = '\0';
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+    return TRY_PARSE_TOKEN_ERROR;
 }
 
-//TODO: testar
-int try_parse_variable(char *variable_string, int variable_string_length, Token *token) {
-    int pos = 0;
-}
 
 //TODO: testar
 //TODO: corrigir comentários (tipo de variável -> tipo de dado)
-int try_parse_data_data(char *variable, TokenType *token_type) {
+int try_parse_data_type(char *variable, TokenType *token_type) {
     if (token_type == NULL) {
         fprintf(stderr, "Erro: ponteiro token_type é NULL.\n");
         return TRY_PARSE_DATA_TYPE_TOKEN_ERROR;
@@ -272,3 +370,198 @@ int try_parse_variable(char *variable_string, int variable_string_length, TokenT
 
     return TRY_PARSE_VARIABLE_TOKEN_SUCCESS;
 }
+
+//TODO: testar
+int try_parse_special_token(char *token_string, int token_string_length, TokenType *token_type) {
+    #pragma region programação defensiva
+    if (token_type == NULL) {
+        fprintf(stderr, "Erro: ponteiro token_type é NULL.\n");
+        return TRY_PARSE_SPECIAL_TOKEN_ERROR;
+    }
+    if (token_string == NULL || token_string_length <= 0) {
+        fprintf(stderr, "Erro: string de token inválida.\n");
+        return TRY_PARSE_SPECIAL_TOKEN_ERROR;
+    }
+    #pragma endregion
+
+    // 1. Verifica se é um parêntese aberto
+    if (strncmp(token_string, "(", 1) == 0) {
+        *token_type = TOKEN_PARENTESES_ABRIR;
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+
+    // 2. Verifica se é um parêntese fechado
+    if (strncmp(token_string, ")", 1) == 0) {
+        *token_type = TOKEN_PARENTESES_FECHAR;
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+
+    // 3. Verifica se é uma chave aberta
+    if (strncmp(token_string, "{", 1) == 0) {
+        *token_type = TOKEN_CHAVE_ABRIR;
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+
+    // 4. Verifica se é uma chave fechada
+    if (strncmp(token_string, "}", 1) == 0) {
+        *token_type = TOKEN_CHAVE_FECHAR;
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+
+    // 5. Verifica se é uma vírgula
+    if (strncmp(token_string, ",", 1) == 0) {
+        *token_type = TOKEN_VIRGULA;
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+
+    // 6. Verifica se é um ponto e vírgula
+    if (strncmp(token_string, ";", 1) == 0) {
+        *token_type = TOKEN_PONTO_E_VIRGULA;
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+
+    // 7. Verifica se é um colchete aberto
+    if (strncmp(token_string, "[", 1) == 0) {
+        *token_type = TOKEN_COCHETE_ABRIR;
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+
+    // 8. Verifica se é um colchete fechado
+    if (strncmp(token_string, "]", 1) == 0) {
+        *token_type = TOKEN_COCHETE_FECHAR;
+        return TRY_PARSE_SPECIAL_TOKEN_SUCCESS;
+    }
+
+    // 9. Se chegou aqui, token especial desconhecido
+    return TRY_PARSE_SPECIAL_TOKEN_NOT_FOUND;
+}
+
+//TODO: testar
+int try_parse_arithmetic_operator(char *operator_string, int operator_string_length, TokenType *token_type) {
+    #pragma region programação defensiva
+    if (token_type == NULL) {
+        fprintf(stderr, "Erro: ponteiro token_type é NULL.\n");
+        return TRY_PARSE_ARITHMETIC_OPERATOR_TOKEN_ERROR;
+    }
+    if (operator_string == NULL || operator_string_length <= 0) {
+        fprintf(stderr, "Erro: string de operador inválida.\n");
+        return TRY_PARSE_ARITHMETIC_OPERATOR_TOKEN_ERROR;
+    }
+    #pragma endregion
+
+    // 1. Verifica se é um operador de adição
+    if (strncmp(operator_string, "+", 1) == 0) {
+        *token_type = TOKEN_OPERADOR_SOMA;
+        return TRY_PARSE_ARITHMETIC_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 2. Verifica se é um operador de subtração
+    if (strncmp(operator_string, "-", 1) == 0) {
+        *token_type = TOKEN_OPERADOR_SUBTRACAO;
+        return TRY_PARSE_ARITHMETIC_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 3. Verifica se é um operador de multiplicação
+    if (strncmp(operator_string, "*", 1) == 0) {
+        *token_type = TOKEN_OPERADOR_MULTIPLICACAO;
+        return TRY_PARSE_ARITHMETIC_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 4. Verifica se é um operador de divisão
+    if (strncmp(operator_string, "/", 1) == 0) {
+        *token_type = TOKEN_OPERADOR_DIVISAO;
+        return TRY_PARSE_ARITHMETIC_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 5. Verifica se é um operador de exponenciação
+    if (strncmp(operator_string, "^", 1) == 0) {
+        *token_type = TOKEN_OPERADOR_EXPONENCIACAO;
+        return TRY_PARSE_ARITHMETIC_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 6. Se chegou aqui, operador aritmético desconhecido
+    return TRY_PARSE_ARITHMETIC_OPERATOR_TOKEN_NOT_FOUND;
+}
+
+TODO: testar
+int try_parse_comparison_operator(char *operator_string, int operator_string_length, TokenType *token_type) {
+    #pragma region programação defensiva
+    if (token_type == NULL) {
+        fprintf(stderr, "Erro: ponteiro token_type é NULL.\n");
+        return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_ERROR;
+    }
+    if (operator_string == NULL || operator_string_length <= 0) {
+        fprintf(stderr, "Erro: string de operador inválida.\n");
+        return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_ERROR;
+    }
+    #pragma endregion
+
+    // 1. Verifica se é um operador de comparação de igualdade
+    if (strncmp(operator_string, "==", 2) == 0) {
+        *token_type = TOKEN_OPERADOR_COMPARACAO_IGUAL;
+        return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 2. Verifica se é um operador de comparação de diferença
+    if (strncmp(operator_string, "!=", 2) == 0) {
+        *token_type = TOKEN_OPERADOR_COMPARACAO_DIFERENTE;
+        return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 3. Verifica se é um operador de comparação maior que
+    if (strncmp(operator_string, ">", 1) == 0) {
+        *token_type = TOKEN_OPERADOR_COMPARACAO_MAIOR;
+        return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 4. Verifica se é um operador de comparação maior ou igual a
+    if (strncmp(operator_string, ">=", 2) == 0) {
+        *token_type = TOKEN_OPERADOR_COMPARACAO_MAIOR_IGUAL;
+        return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 5. Verifica se é um operador de comparação menor que
+    if (strncmp(operator_string, "<", 1) == 0) {
+        *token_type = TOKEN_OPERADOR_COMPARACAO_MENOR;
+        return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 6. Verifica se é um operador de comparação menor ou igual a
+    if (strncmp(operator_string, "<=", 2) == 0) {
+        *token_type = TOKEN_OPERADOR_COMPARACAO_MENOR_IGUAL;
+        return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 7. Se chegou aqui, operador de comparação desconhecido
+    return TRY_PARSE_COMPARISON_OPERATOR_TOKEN_NOT_FOUND;
+}
+
+TODO: testar
+int try_parse_logical_operator(char *operator_string, int operator_string_length, TokenType *token_type) {
+    #pragma region programação defensiva
+    if (token_type == NULL) {
+        fprintf(stderr, "Erro: ponteiro token_type é NULL.\n");
+        return TRY_PARSE_LOGICAL_OPERATOR_TOKEN_ERROR;
+    }
+    if (operator_string == NULL || operator_string_length <= 0) {
+        fprintf(stderr, "Erro: string de operador lógico inválida.\n");
+        return TRY_PARSE_LOGICAL_OPERATOR_TOKEN_ERROR;
+    }
+    #pragma endregion
+
+    // 1. Verifica se é um operador lógico E
+    if (strncmp(operator_string, "&&", 2) == 0) {
+        *token_type = TOKEN_OPERADOR_LOGICO_E;
+        return TRY_PARSE_LOGICAL_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 2. Verifica se é um operador lógico OU
+    if (strncmp(operator_string, "||", 2) == 0) {
+        *token_type = TOKEN_OPERADOR_LOGICO_OU;
+        return TRY_PARSE_LOGICAL_OPERATOR_TOKEN_SUCCESS;
+    }
+
+    // 3. Se chegou aqui, operador lógico desconhecido
+    return TRY_PARSE_LOGICAL_OPERATOR_TOKEN_NOT_FOUND;
+}
+
